d39000d99065e175456f9967685d0cbe7f20b880
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-09-10 20:19:49 -0300

    FEAT: Leitura do banco de dados

--- api/ler.php
+++ api/ler.php
@@ -7,13 +7,13 @@ $json = json_decode(file_get_contents("php://input"), true);
 $identificador = $conn->real_escape_string($json["opcao"]);
 
 if ($identificador === "clientes") {
-    $sql = "SELECT * FROM clientes ORDER BY nome_cliente ASC, ativo DESC";
+    $sql = "SELECT * FROM clientes ORDER BY ativo DESC, nome_cliente ASC";
 } elseif ($identificador === "livros") {
-    $sql = "SELECT * FROM livros ORDER BY nome_livro DESC";
+    $sql = "SELECT * FROM livros ORDER BY nome_livro ASC";
 } elseif ($identificador === "alugueis") {
-    $sql = "";
+    $sql = "SELECT id_alugados, nome_cliente, cpf, nome_livro, codigo, data_aluguel, data_devolucao, situacao FROM alugados JOIN clientes ON alugados.id_cliente = clientes.id_cliente JOIN livros ON alugados.id_livro = livros.id_livro";
 } else {
-    $sql = "";
+    $sql = "Erro!";
 }
 
 if ($sql != "Erro!") {
