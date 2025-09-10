e3a412075425f2f177f1a1c61f9b28058202a2c0
Gilber-Mattos-Mendes <ggmattosjf@gmail.com>
2025-09-10 20:06:31 -0300

    Banco Correto

---
+++ db/horizonte_das_palavras (1).sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 11/09/2025 às 01:05
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `horizonte_das_palavras`
+--
+CREATE DATABASE IF NOT EXISTS `horizonte_das_palavras` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `horizonte_das_palavras`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `alugados`
+--
+
+CREATE TABLE IF NOT EXISTS `alugados` (
+  `id_alugados` int(11) NOT NULL AUTO_INCREMENT,
+  `id_cliente` int(11) NOT NULL,
+  `id_livro` int(11) NOT NULL,
+  `data_aluguel` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_devolucao` varchar(100) DEFAULT NULL,
+  `situacao` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id_alugados`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_livro` (`id_livro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_cliente` varchar(100) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `cpf` varchar(15) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `ativo` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `livros`
+--
+
+CREATE TABLE IF NOT EXISTS `livros` (
+  `id_livro` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_livro` varchar(100) NOT NULL,
+  `codigo` int(11) DEFAULT NULL,
+  `quantidade_disponivel` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_livro`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `alugados`
+--
+ALTER TABLE `alugados`
+  ADD CONSTRAINT `alugados_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`),
+  ADD CONSTRAINT `alugados_ibfk_2` FOREIGN KEY (`id_livro`) REFERENCES `livros` (`id_livro`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
