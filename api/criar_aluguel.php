f0a98f9db1925e0ea88056aa2bbdecce8c25a3f1
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-09-12 20:57:44 -0300

    FEAT: Correção na criação

--- api/criar_aluguel.php
+++ api/criar_aluguel.php
@@ -1,17 +1,24 @@
 <?php
+// Define o tipo de aplicação
 header("Content-Type: application/json");
+// Inclui a conexão com o banco de dados
 include("../db/conexao.php");
 
+// Decodifica o json enviado pelo JavaScript
 $dados = json_decode(file_get_contents("php://input"), true);
 
+// Converte os dados do json pro tipo necessário
 $nomeCliente = $conn->real_escape_string($dados['nome-cliente']);
 $nomeLivro = $conn->real_escape_string($dados['nome-livro']);
 $dataAluguel = $conn->real_escape_string($dados['data-aluguel']);
 $dataDevolucao = $conn->real_escape_string($dados['data-devolucao']);
 
-$idCliente = "SELECT id_cliente FROM clientes WHERE $nomeCliente"
-$idLivro = "SELECT id_livro FROM livros WHERE $nomeLivro"
+// Gera querys SQL para recuperar os IDs do cliente e do livro pelos seus nomes
+$idCliente = $conn->query("SELECT id_cliente FROM clientes WHERE $nomeCliente");
+$idLivro = $conn->query("SELECT id_livro FROM livros WHERE $nomeLivro");
 
+// Gera a query de inserção desses dados
 $sql = "INSERT INTO alugados (id_cliente, id_livro, data_aluguel, data_devolucao) VALUES ($idCliente, $idLivro, $dataAluguel, $dataDevolucao)"
 
+$conn->query($sql);
 ?>
