c6c4e029398d158f5b557e5043731099d272135c
AgataQuadros <quadrosagata@gmail>
2025-09-12 20:33:52 -0300

    FEAT: Concertando o Criar Aluguel, Editando Algumas Paginas PHP

--- api/criar_aluguel.php
+++ api/criar_aluguel.php
@@ -6,12 +6,11 @@ $dados = json_decode(file_get_contents("php://input"), true);
 
 $nomeCliente = $conn->real_escape_string($dados['nome-cliente']);
 $nomeLivro = $conn->real_escape_string($dados['nome-livro']);
-$dataAluguel = $conn->real_escape_string($dados['data-aluguel']);
 $dataDevolucao = $conn->real_escape_string($dados['data-devolucao']);
 
-$idCliente = "SELECT id_cliente FROM clientes WHERE $nomeCliente"
-$idLivro = "SELECT id_livro FROM livros WHERE $nomeLivro"
+$idCliente = "SELECT id_cliente FROM clientes WHERE nome_cliente = $nomeCliente"
+$idLivro = "SELECT id_livro FROM livros WHERE nome_livro = $nomeLivro"
 
-$sql = "INSERT INTO alugados (id_cliente, id_livro, data_aluguel, data_devolucao) VALUES ($idCliente, $idLivro, $dataAluguel, $dataDevolucao)"
+$sql = "INSERT INTO alugados (id_cliente, id_livro, data_devolucao) VALUES ($idCliente, $idLivro, $dataDevolucao)"
 
 ?>
