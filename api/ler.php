523b7099cbbece588ffc7065181ebf63046b5ea7
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-09-12 20:57:59 -0300

    FEAT: Comentários na leitura

--- api/ler.php
+++ api/ler.php
@@ -1,11 +1,16 @@
 <?php
+// Define o tipo de aplicação
 header("Content-Type: application/json");
+// Inclui a conexão com o banco de dados
 include "../db/conexao.php";
 
+// Decodifica o json enviado pelo JavaScript
 $json = json_decode(file_get_contents("php://input"), true);
 
+// Pega o identificador do json
 $identificador = $conn->real_escape_string($json["opcao"]);
 
+// Verifica qual o identificador e gera a query SQL de acordo
 if ($identificador === "clientes") {
     $sql = "SELECT * FROM clientes ORDER BY ativo DESC, nome_cliente ASC";
 } elseif ($identificador === "livros") {
@@ -17,6 +22,7 @@ if ($identificador === "clientes") {
 }
 
 if ($sql != "Erro!") {
+    // Se não houver erro na opção, executa a query SQL
     $result = $conn->query($sql);
 
     $saidas = [];
@@ -25,8 +31,10 @@ if ($sql != "Erro!") {
         $saidas[] = $row;
     }
 } else {
+    // Se houver erro, envia a mensagem de erro
     $saidas[] = "ERRO: Opção de leitura inválida!"
 }
 
+// Codifica a resposta e envia
 echo json_encode($saidas);
 ?>
