1e6e819c02d60821f1c73a4d7a7b592857a6d8ea
AgataQuadros <quadrosagata@gmail>
2025-09-22 19:15:56 -0300

    FEAT: Tentando Arrumar a Pagina de Editar Cliente

--- editar.php
+++ editar.php
@@ -18,7 +18,7 @@
     </header>
     <main>
 
-        <section class="lista">
+        <section class="lista-editar">
 
                 <table>
 
@@ -39,7 +39,7 @@
                 </table>
         </section>
 
-        <section class="lista">
+        <section class="lista-editar">
                 
                 <table>
                     
