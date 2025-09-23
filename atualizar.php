852b566badc953da5b0e469532d7541e35328319
AgataQuadros <quadrosagata@gmail>
2025-09-23 19:30:26 -0300

    FEAT: Organizando a Formatação da Pagina Atualizar

--- atualizar.php
+++ atualizar.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar</title>
+    <link rel="stylesheet" href="public/css/estilo.css">
 </head>
 <body>
     <header class="principal">
@@ -15,21 +16,21 @@
 
     <main>
         <section>
-            <article>
+            <article class="atualizar">
                 <form id="formAtualizar" method="POST">
                     <h2>Atualizar Cliente</h2>
 
-                    <label for="nome-cliente">Nome</label>
-                    <input type="text" id="nome-cliente" name="nome-cliente">
+                    <label for="nome-editar">Nome</label>
+                    <input type="text" id="nome-editar" name="nome-editar">
 
-                    <label for="telefone">Telefone</label>
-                    <input type="text" id="telefone" name="telefone">
+                    <label for="telefone-editar">Telefone</label>
+                    <input type="text" id="telefone-editar" name="telefone-editar">
 
-                    <label for="cpf">CPF</label>
-                    <input type="text" id="cpf" name="cpf">
+                    <label for="cpf-editar">CPF</label>
+                    <input type="text" id="cpf-editar" name="cpf-editar">
 
-                    <label for="aniversario">Data de Nascimento</label>
-                    <input type="date" id="aniversario" name="aniversario">
+                    <label for="aniversario-editar">Data de Nascimento</label>
+                    <input type="date" id="aniversario-editar" name="aniversario-editar">
                     <button type="submit">Atualizar Cliente</button>
                 </form>
             </article>
